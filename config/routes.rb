415dcfcf1067b31bf8dc98e8200488dda7e510a3
depippo <jdepippo@gmail.com>
2017-07-23 16:44:01 -0400

    Added link to restricted players list from main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :players
   root 'standings#index'
-  get 'players => players#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
