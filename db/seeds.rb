a9aab58a55e87cb31935d1e09e71a1980530ac80
depippo <jdepippo@gmail.com>
2017-07-23 16:21:05 -0400

    Ability to add and remove players, list sorted alphabetically

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-ymoncada = Player.create!(:name => "Yoan Moncada")
-kallard = Player.create!(:name => "Kolby Allard")
